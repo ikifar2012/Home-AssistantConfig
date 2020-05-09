fa35237a6d3654e157fe6666968ce5222ab1cb1a
Matheson Steplock <ikifar2012@users.noreply.github.com>
2020-05-08 23:33:52 -0400

    updates and rain automation fixes - RIP Dark Sky

--- custom_components/alexa_media/const.py
+++ custom_components/alexa_media/const.py
@@ -9,7 +9,7 @@ https://community.home-assistant.io/t/echo-devices-alexa-as-media-player-testers
 """
 from datetime import timedelta
 
-__version__ = "2.5.13"
+__version__ = "2.6.1"
 PROJECT_URL = "https://github.com/custom-components/alexa_media_player/"
 ISSUE_URL = "{}issues".format(PROJECT_URL)
 
@@ -21,13 +21,20 @@ SCAN_INTERVAL = timedelta(seconds=60)
 MIN_TIME_BETWEEN_SCANS = SCAN_INTERVAL
 MIN_TIME_BETWEEN_FORCED_SCANS = timedelta(seconds=1)
 
-ALEXA_COMPONENTS = ["media_player", "notify", "alarm_control_panel", "switch", "sensor"]
+ALEXA_COMPONENTS = [
+    "media_player",
+]
+DEPENDENT_ALEXA_COMPONENTS = ["notify", "switch", "sensor", "alarm_control_panel"]
 
 CONF_ACCOUNTS = "accounts"
 CONF_DEBUG = "debug"
 CONF_INCLUDE_DEVICES = "include_devices"
 CONF_EXCLUDE_DEVICES = "exclude_devices"
+CONF_QUEUE_DELAY = "queue_delay"
 
+DATA_LISTENER = "listener"
+
+DEFAULT_QUEUE_DELAY = 1.5
 SERVICE_CLEAR_HISTORY = "clear_history"
 SERVICE_UPDATE_LAST_CALLED = "update_last_called"
 ATTR_MESSAGE = "message"
