4bd0a242a668058031743f8bba75f5ce6b58ab75
Matheson Steplock <ikifar2012@users.noreply.github.com>
2021-10-11 22:04:56 -0400

    upgrade to reolink_dev

---
+++ custom_components/reolink_dev/__init__.py
@@ -0,0 +1,162 @@
+"""Reolink integration for HomeAssistant."""
+import asyncio
+from datetime import timedelta
+import logging
+
+import async_timeout
+
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.const import (
+    CONF_HOST,
+    CONF_PASSWORD,
+    CONF_PORT,
+    CONF_TIMEOUT,
+    CONF_USERNAME,
+    EVENT_HOMEASSISTANT_STOP,
+)
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers import device_registry
+from homeassistant.helpers.storage import STORAGE_DIR
+from homeassistant.helpers.update_coordinator import DataUpdateCoordinator
+
+from .base import ReolinkBase, ReolinkPush
+from .const import (
+    BASE,
+    CONF_CHANNEL,
+    CONF_MOTION_OFF_DELAY,
+    CONF_PLAYBACK_MONTHS,
+    CONF_PROTOCOL,
+    CONF_STREAM,
+    CONF_THUMBNAIL_PATH,
+    CONF_STREAM_FORMAT,
+    COORDINATOR,
+    DOMAIN,
+    EVENT_DATA_RECEIVED,
+    PUSH_MANAGER,
+    SERVICE_PTZ_CONTROL,
+    SERVICE_QUERY_VOD,
+    SERVICE_SET_DAYNIGHT,
+    SERVICE_SET_SENSITIVITY,
+)
+
+SCAN_INTERVAL = timedelta(minutes=1)
+
+
+_LOGGER = logging.getLogger(__name__)
+
+PLATFORMS = ["camera", "switch", "binary_sensor", "sensor"]
+
+
+async def async_setup(
+    hass: HomeAssistant, config: dict
+):  # pylint: disable=unused-argument
+    """Set up the Reolink component."""
+    hass.data.setdefault(DOMAIN, {})
+
+    # ensure default storage path is writable by scripts
+    default_thumbnail_path = hass.config.path(f"{STORAGE_DIR}/{DOMAIN}")
+    if default_thumbnail_path not in hass.config.allowlist_external_dirs:
+        hass.config.allowlist_external_dirs.add(default_thumbnail_path)
+
+    return True
+
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Set up Reolink from a config entry."""
+
+    hass.data.setdefault(DOMAIN, {})
+
+    base = ReolinkBase(hass, entry.data, entry.options)
+    base.sync_functions.append(entry.add_update_listener(update_listener))
+
+    if not await base.connect_api():
+        return False
+    hass.data[DOMAIN][entry.entry_id] = {BASE: base}
+
+    try:
+        """Get a push manager, there should be one push manager per mac address"""
+        push = hass.data[DOMAIN][base.push_manager]
+    except KeyError:
+        push = ReolinkPush(
+            hass,
+            base.api.host,
+            base.api.onvif_port,
+            entry.data[CONF_USERNAME],
+            entry.data[CONF_PASSWORD],
+        )
+        await push.subscribe(base.event_id)
+        hass.data[DOMAIN][base.push_manager] = push
+
+    async def async_update_data():
+        """Perform the actual updates."""
+
+        async with async_timeout.timeout(base.timeout):
+            await push.renew()
+            await base.update_states()
+
+    coordinator = DataUpdateCoordinator(
+        hass,
+        _LOGGER,
+        name="reolink",
+        update_method=async_update_data,
+        update_interval=SCAN_INTERVAL,
+    )
+
+    # Fetch initial data so we have data when entities subscribe
+    await coordinator.async_refresh()
+
+    for component in PLATFORMS:
+        hass.async_create_task(
+            hass.config_entries.async_forward_entry_setup(entry, component)
+        )
+
+    hass.data[DOMAIN][entry.entry_id][COORDINATOR] = coordinator
+
+    hass.bus.async_listen_once(EVENT_HOMEASSISTANT_STOP, base.stop())
+
+    return True
+
+
+async def update_listener(hass: HomeAssistant, entry: ConfigEntry):
+    """Update the configuration at the base entity and API."""
+    base: ReolinkBase = hass.data[DOMAIN][entry.entry_id][BASE]
+
+    base.motion_off_delay = entry.options[CONF_MOTION_OFF_DELAY]
+    base.playback_months = entry.options[CONF_PLAYBACK_MONTHS]
+
+    base.set_thumbnail_path(entry.options.get(CONF_THUMBNAIL_PATH))
+    await base.set_timeout(entry.options[CONF_TIMEOUT])
+    await base.set_protocol(entry.options[CONF_PROTOCOL])
+    await base.set_stream(entry.options[CONF_STREAM])
+    await base.set_stream_format(entry.options[CONF_STREAM_FORMAT])
+
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
+    """Unload a config entry."""
+    base = hass.data[DOMAIN][entry.entry_id][BASE]
+    push = hass.data[DOMAIN][base.push_manager]
+
+    if not await push.count_members() > 1:
+        await push.unsubscribe()
+        hass.data[DOMAIN].pop(base.push_manager)
+
+    await base.stop()
+
+    unload_ok = all(
+        await asyncio.gather(
+            *[
+                hass.config_entries.async_forward_entry_unload(entry, component)
+                for component in PLATFORMS
+            ]
+        )
+    )
+    if unload_ok:
+        hass.data[DOMAIN].pop(entry.entry_id)
+
+    if len(hass.data[DOMAIN]) == 0:
+        hass.services.async_remove(DOMAIN, SERVICE_PTZ_CONTROL)
+        hass.services.async_remove(DOMAIN, SERVICE_SET_DAYNIGHT)
+        hass.services.async_remove(DOMAIN, SERVICE_SET_SENSITIVITY)
+        hass.services.async_remove(DOMAIN, SERVICE_QUERY_VOD)
+
+    return unload_ok
---
+++ custom_components/reolink_dev/binary_sensor.py
@@ -0,0 +1,260 @@
+"""This component provides support for Reolink motion events."""
+import asyncio
+import datetime
+import logging
+
+from homeassistant.components.binary_sensor import BinarySensorEntity
+
+from .entity import ReolinkEntity
+
+_LOGGER = logging.getLogger(__name__)
+
+DEFAULT_DEVICE_CLASS = "motion"
+
+
+async def async_setup_entry(hass, config_entry, async_add_devices):
+    """Set up the Reolink IP Camera switches."""
+    sensor = MotionSensor(hass, config_entry)
+
+    person_detected_sensor = ObjectDetectedSensor(hass, config_entry, 'person')
+    vehicle_detected_sensor = ObjectDetectedSensor(hass, config_entry, 'vehicle')
+    pet_detected_sensor = ObjectDetectedSensor(hass, config_entry, 'pet')
+
+    async_add_devices([sensor, person_detected_sensor, vehicle_detected_sensor, pet_detected_sensor],
+                      update_before_add=False)
+
+
+class MotionSensor(ReolinkEntity, BinarySensorEntity):
+    """An implementation of a Reolink IP camera motion sensor."""
+
+    def __init__(self, hass, config):
+        """Initialize a the switch."""
+        ReolinkEntity.__init__(self, hass, config)
+        BinarySensorEntity.__init__(self)
+
+        self._available = False
+        self._event_state = False
+        self._last_event_state = False
+        self._last_motion = datetime.datetime.min
+
+    @property
+    def unique_id(self):
+        """Return Unique ID string."""
+        return f"reolink_motion_{self._base.unique_id}"
+
+    @property
+    def name(self):
+        """Return the name of this camera."""
+        return f"{self._base.name} motion"
+
+    @property
+    def is_on(self):
+        """Return the state of the sensor."""
+        if not self._base.motion_detection_state:
+            self._state = False
+            return self._state
+
+        if self._event_state or self._base.motion_off_delay == 0:
+            self._state = self._event_state
+            return self._state
+
+        if (
+            datetime.datetime.now() - self._last_motion
+        ).total_seconds() < self._base.motion_off_delay:
+            self._state = True
+        else:           
+            self._state = False
+
+        return self._state
+
+    @property
+    def available(self):
+        """Return True if entity is available."""
+        return self._available
+
+    @property
+    def device_class(self):
+        """Return the class of this device."""
+        return DEFAULT_DEVICE_CLASS
+
+    async def async_added_to_hass(self) -> None:
+        """Entity created."""
+        await super().async_added_to_hass()
+        self.hass.bus.async_listen(self._base.event_id, self.handle_event)
+
+    async def handle_event(self, event):
+        """Handle incoming event for motion detection and availability."""
+
+        try:
+            self._available = event.data["available"]
+            return
+        except KeyError:
+            pass
+
+        if not self._available:
+            return
+
+        try:
+            self._last_event_state = bool(self._event_state)
+            self._event_state = event.data["motion"]
+        except KeyError:
+            return
+
+        if self._base.api.channels > 1:
+            # Pull the motion state for the NVR channel, it has only 1 event
+            self._event_state = await self._base.api.get_motion_state()
+
+        if self._event_state:
+            self._last_motion = datetime.datetime.now()
+
+            if self._base.api.ai_state:
+                # Pull the AI state only at motion detection
+                await self._base.api.get_ai_state()
+                # send an event to AI based motion sensor entities
+                self._hass.bus.async_fire(self._base.event_id, {"ai_refreshed": True})
+        else:
+            if self._base.motion_off_delay > 0:
+                await asyncio.sleep(self._base.motion_off_delay)
+
+        self.async_schedule_update_ha_state()
+
+    @property
+    def extra_state_attributes(self):
+        """Return the state attributes."""
+        attrs = super().extra_state_attributes
+
+        if attrs is None:
+            attrs = {}
+
+        attrs["bus_event_id"] = self._base.event_id
+
+        if self._base.api.ai_state:
+            for key, value in self._base.api.ai_state.items():
+                if key == "channel":
+                    continue
+
+                if self._state:
+                    if isinstance(value, int):  # compatibility with firmware < 3.0.0-494
+                        attrs[key] = value == 1
+                    else:
+                        # from firmware 3.0.0.0-494 there is a new json structure:
+                        # [
+                        #     {
+                        #         "cmd" : "GetAiState",
+                        #         "code" : 0,
+                        #         "value" : {
+                        #             "channel" : 0,
+                        #             "face" : {
+                        #                 "alarm_state" : 0,
+                        #                 "support" : 0
+                        #             },
+                        #             "people" : {
+                        #                 "alarm_state" : 0,
+                        #                 "support" : 1
+                        #             },
+                        #             "vehicle" : {
+                        #                 "alarm_state" : 0,
+                        #                 "support" : 1
+                        #             }
+                        #         }
+                        #     }
+                        # ]
+                        attrs[key] = value.get("alarm_state", 0) == 1
+                else:
+                    # Reset the AI values.
+                    attrs[key] = False
+
+        return attrs
+
+
+class ObjectDetectedSensor(ReolinkEntity, BinarySensorEntity):
+    """An implementation of a Reolink IP camera object motion sensor."""
+
+    def __init__(self, hass, config, object_type: str):
+        """Initialize a the switch."""
+        ReolinkEntity.__init__(self, hass, config)
+        BinarySensorEntity.__init__(self)
+
+        self._available = False
+        self._event_state = False
+        self._last_event_state = False
+        self._last_motion = datetime.datetime.min
+        self._object_type = object_type
+
+
+    @property
+    def unique_id(self):
+        """Return Unique ID string."""
+        return f"reolink_object_{self._object_type}_detected_{self._base.unique_id}"
+
+    @property
+    def name(self):
+        """Return the name of this sensor."""
+        return f"{self._base.name} {self._object_type} detected"
+
+    @property
+    def is_on(self):
+        """Return the state of the sensor."""
+        self._state = self._event_state
+
+        return self._state
+
+    @property
+    def available(self):
+        """Return True if entity is available."""
+        return self._available
+
+    @property
+    def device_class(self):
+        """Return the class of this device."""
+        return DEFAULT_DEVICE_CLASS
+
+    async def async_added_to_hass(self) -> None:
+        """Entity created."""
+        await super().async_added_to_hass()
+        self.hass.bus.async_listen(self._base.event_id, self.handle_event)
+
+    async def handle_event(self, event):
+        """Handle incoming event for motion detection and availability."""
+
+        try:
+            self._available = event.data["available"]
+            return
+        except KeyError:
+            pass
+
+        if event.data.get("ai_refreshed") is not True:
+            return
+
+        self._last_event_state = bool(self._event_state)
+        self._event_state = False
+
+        if self._base.api.ai_state:
+            object_found = False
+            for key, value in self._base.api.ai_state.items():
+                if key == "channel":
+                    continue
+
+                if key == self._object_type or (self._object_type == 'person' and key == 'people'):
+                    if isinstance(value, int):  # compatibility with firmware < 3.0.0-494
+                        self._event_state = value == 1
+                    else:
+                        self._event_state = value.get('alarm_state', 0) == 1
+                        self._available = value.get('support', 0) == 1
+
+                    self.async_schedule_update_ha_state()
+                    object_found = True
+                    break
+
+            if not object_found:
+                self._available = False
+
+
+
+
+
+
+
+
+
+
